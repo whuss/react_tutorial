3a65a99ca5d5d0a8feb870a5553dde745d52489a
Wilfried Huss <husswilfried@gmail.com>
2021-01-19 13:47:47 +0100

    add counter

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,6 @@ module.exports = {
     semi: true,
     trailingComma: "es5",
     singleQuote: false,
-    printWidth: 120,
+    printWidth: 80,
     tabWidth: 4,
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
+import { Button, TextField } from "@material-ui/core";
 import "./App.css";
 
+const style = { background: "green", padding: 10, marginBotton: 10 };
+
 function App() {
+    const [counter, setCounter] = useState(0);
+
+    const increaseCounter = () => {
+        setCounter(counter + 1);
+    };
+
+    const decreaseCounter = () => {
+        setCounter(counter - 1);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
+                <div style={style}>Counter: {counter}</div>
+                <Button
+                    onClick={increaseCounter}
+                    variant="contained"
+                    color="secondary"
+                >
+                    Increase Counter
+                </Button>
+                <Button
+                    onClick={decreaseCounter}
+                    variant="contained"
+                    color="primary"
+                >
+                    Decrease Counter
+                </Button>
             </header>
         </div>
     );
