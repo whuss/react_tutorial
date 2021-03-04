3c7fdf0bb760fe36cffd6fdf3f2a54083f1844f7
Wilfried Huss <husswilfried@gmail.com>
2021-03-04 09:31:49 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,34 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import { Button, TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import "./App.css";
 
-const style = { background: "green", padding: 10, marginBotton: 10 };
+export const style = { background: "green", padding: 10, marginBotton: 10 };
 
-function App() {
-    const [counter, setCounter] = useState(0);
+type CounterProps = {
+    counter: number;
+    increaseCounter: () => void;
+};
+const Counter: React.FC<CounterProps> = (props) => {
+    return (
+        <>
+            <Button onClick={props.increaseCounter} variant="contained" color="secondary">
+                Increase Counter
+            </Button>
+        </>
+    );
+};
 
-    const increaseCounter = () => {
-        setCounter(counter + 1);
-    };
-
-    const decreaseCounter = () => {
-        setCounter(counter - 1);
-    };
+const App: React.FC = () => {
+    const [counter1, setCounter1] = useState(0);
+    const [counter2, setCounter2] = useState(0);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <div style={style}>Counter: {counter}</div>
-                <Button
-                    onClick={increaseCounter}
-                    variant="contained"
-                    color="secondary"
-                >
-                    Increase Counter
-                </Button>
-                <Button
-                    onClick={decreaseCounter}
-                    variant="contained"
-                    color="primary"
-                >
-                    Decrease Counter
-                </Button>
-            </header>
-        </div>
+        <>
+            <Counter counter={counter1} increaseCounter={() => setCounter1(counter1 + 1)} />
+            <div>Counter 1: {counter1}</div>
+            <Counter counter={counter2} increaseCounter={() => setCounter2(counter2 + 1)} />
+        </>
     );
-}
+};
 
 export default App;
